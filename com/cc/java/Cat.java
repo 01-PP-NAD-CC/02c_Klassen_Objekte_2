83726079f7fbc251b8ca81f714ea1fca2f0561de
Peter <schmidt.multimedia@gmail.com>
2024-01-29 10:03:17 +0100

    Tomcat / Cat

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -5,15 +5,12 @@ public class Cat {
   private String name;
   private String furColor;
   private int age; 
-  private boolean isFemale;
-
   private int counter;
 
-  public Cat(String name, String furColor, int age, boolean isFemale) {
+  public Cat(String name, String furColor, int age) {
     this.name = name;
     this.furColor = furColor;
     this.age = age;
-    this.isFemale = isFemale;
   }
 
   public String getStringAttributes(String op) {
@@ -28,32 +25,25 @@ public class Cat {
   }
 
   public String getAge() {
-    if (isFemale) {
-      return checkCompliance();
-    } else {
-      return String.valueOf(age);
+    return checkCompliance();
+  }
+
+  private String checkCompliance(){
+
+    counter++;
+
+    switch (counter) {
+      case 1:
+        return "This is an inappropriate question!";
+      case 2:
+        return "I've told you once!";
+      case 3:
+        return "Talk to the hand!";
+      default:
+        return "1#!?&&%";
     }
+    
   }
 
-private String checkCompliance(){
-
-  counter++;
-
-  switch (counter) {
-    case 1:
-      return "This is an inappropriate question!";
-    case 2:
-      return "I've told you once!";
-    case 3:
-      return "Talk to the hand!";
-    default:
-      return "1#!?&&%";
-  }
-  
-}
-
-
-
-  
 }
   
